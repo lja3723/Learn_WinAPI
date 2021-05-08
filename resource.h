378b026f524c4a534f4abe2a95c49b1a399503d7
이우현 <lja3333@hotmail.com>
2021-05-08 11:18:39 +0900

    mod ex4.1.rc.menu

--- resource.h
+++ resource.h
@@ -3,13 +3,18 @@
 // Learn_WinAPI.rc에서 사용되고 있습니다.
 //
 #define IDR_MENU1                       101
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
+#define ID_40004                        40004
+#define ID_40005                        40005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_COMMAND_VALUE         40006
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
