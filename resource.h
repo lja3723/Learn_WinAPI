9f7097727cba1a23f580cb4f7c0ce0a9b29a7fe8
이우현 <lja3333@hotmail.com>
2021-05-08 14:00:18 +0900

    add ex4.2

--- resource.h
+++ resource.h
@@ -2,35 +2,22 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // Learn_WinAPI.rc에서 사용되고 있습니다.
 //
-#define IDR_MENU1                       101
-#define IDR_MENU4_1                     101
-#define ID_40001                        40001
-#define ID_40002                        40002
-#define ID_40003                        40003
-#define ID_40004                        40004
-#define ID_40005                        40005
-#define ID_40006                        40006
-#define ID_40007                        40007
-#define ID_FILENEW                      40008
-#define ID_FILEOPEN                     40009
-#define ID_FILESAVE                     40010
-#define ID_EXIT                         40011
-#define ID_40012                        40012
-#define ID_40013                        40013
-#define ID_40014                        40014
-#define ID_40015                        40015
-#define ID_40016                        40016
-#define ID_EDITUNDO                     40017
-#define ID_EDITCOPY                     40018
-#define ID_EDITPASTE                    40019
-#define ID_INFORM                       40020
+#define IDR_MENU4_2                     101
+#define ID_FILENEW                      40001
+#define ID_FILEOPEN                     40002
+#define ID_FILESAVE                     40003
+#define ID_EDITUNDO                     40004
+#define ID_EDITCOPY                     40005
+#define ID_EDITPASTE                    40006
+#define ID_INFORM                       40007
+#define ID_EXIT                         40008
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40021
+#define _APS_NEXT_COMMAND_VALUE         40009
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
