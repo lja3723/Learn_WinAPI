d62af1ad4a4a471513f013616327c0d55296fdcc
이우현 <lja3333@hotmail.com>
2021-05-09 20:20:08 +0900

    add ex4.9

--- resource.h
+++ resource.h
@@ -4,6 +4,7 @@
 //
 #define IDR_MENU4_5                     101
 #define IDR_MENU4_7                     102
+#define IDR_MENU4_9                     103
 #define ID_FILENEW                      40001
 #define ID_FILEOPEN                     40002
 #define ID_FILESAVE                     40003
@@ -19,7 +20,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40013
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
