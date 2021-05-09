e08ac37f4bb059a43bd2ad5678a18a4cdaaa52c3
lja3723 <lja3723@gmail.com>
2021-05-09 23:12:11 +0900

    add ex6.3

---
+++ ex6-3.cpp
@@ -0,0 +1,133 @@
+#define LearningChapter "ex6-3"
+//page 241
+
+#include <Windows.h>
+#include <tchar.h>
+#include "resource.h"
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+static LPCTSTR WindowClassName = _T(LearningChapter) _T(" Class");
+static LPCTSTR WindowTitleName = _T(LearningChapter);
+static const int WindowPosition[2] = { 400, 200 };
+static const int WindowSize[2] = { 720, 480 };
+
+BOOL CALLBACK Dlg6_2Proc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK Dlg6_3Proc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam);
+HINSTANCE g_hInstance;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS WndClass;
+	g_hInstance = hInstance;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU6_3);
+	WndClass.lpszClassName = WindowClassName;
+
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(WindowClassName, WindowTitleName,
+		WS_OVERLAPPEDWINDOW,
+		WindowPosition[0], WindowPosition[1],
+		WindowSize[0], WindowSize[1],
+		NULL, NULL, hInstance, NULL
+	);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+BOOL CALLBACK Dlg6_3Proc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HWND hButton;
+
+	switch (iMsg)
+	{
+	case WM_INITDIALOG:
+		hButton = GetDlgItem(hDlg, ID_PAUSE);
+		EnableWindow(hButton, FALSE);
+		return 1;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_START:
+			hButton = GetDlgItem(hDlg, ID_START);
+			EnableWindow(hButton, FALSE);
+			hButton = GetDlgItem(hDlg, ID_PAUSE);
+			EnableWindow(hButton, TRUE);
+			break;
+		case ID_PAUSE:
+			hButton = GetDlgItem(hDlg, ID_START);
+			EnableWindow(hButton, TRUE);
+			hButton = GetDlgItem(hDlg, ID_PAUSE);
+			EnableWindow(hButton, FALSE);
+			break;
+		case ID_CLOSE:
+			EndDialog(hDlg, 0);
+			break;
+		case IDCANCEL:
+			EndDialog(hDlg, 0);
+			break;
+		}
+		break;
+	}
+
+	return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg) {
+	case WM_CREATE:
+		return 0;
+	case WM_COMMAND:
+		switch (LOWORD(wParam)){
+		case ID_6_3_MENU:
+			DialogBox(g_hInstance, MAKEINTRESOURCE(IDD_DIALOG6_3), hwnd, (DLGPROC)Dlg6_3Proc);
+			break;
+		}
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+BOOL CALLBACK Dlg6_2Proc(HWND hDlg, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	LPCTSTR str;
+	switch (iMsg)
+	{
+	case WM_INITDIALOG:
+		return 1;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_BUTTON_PRINT:
+			hdc = GetDC(hDlg);
+			str = _T("The Quick Brown Fox Jumps Over the Lazy Dog.");
+			TextOut(hdc, 10, 10, str, _tcslen(str));
+			ReleaseDC(hDlg, hdc);
+			break;
+		case ID_BUTTON_END:
+			EndDialog(hDlg, 0);
+			break;
+		}
+	}
+	return 0;
+}
--- resource.h
+++ resource.h
@@ -7,8 +7,13 @@
 #define IDR_MENU4_9                     103
 #define IDD_DIALOG6_1                   104
 #define IDD_DIALOG6_2                   105
-#define ID_BUTTON_PRINT                 1002
-#define ID_BUTTON_END                   1003
+#define IDR_MENU6_3                     106
+#define IDD_DIALOG6_3                   107
+#define ID_BUTTON_PRINT                 1001
+#define ID_BUTTON_END                   1002
+#define ID_START                        1003
+#define ID_PAUSE                        1004
+#define ID_CLOSE                        1006
 #define ID_FILENEW                      40001
 #define ID_FILEOPEN                     40002
 #define ID_FILESAVE                     40003
@@ -19,14 +24,18 @@
 #define ID_EXIT                         40008
 #define ID_FONTDLG                      40009
 #define ID_COLORDLG                     40010
+#define ID_FILE                         40011
+#define ID_EDIT                         40012
+#define ID_INFO                         40013
+#define ID_6_3_MENU                     40014
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40011
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
