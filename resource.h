89e545aca4c7b029aa9cf6a096e06d2ed57877dc
lja3723 <lja3723@gmail.com>
2021-05-09 23:15:35 +0900

    mod ex6.3.rc.menu

--- resource.h
+++ resource.h
@@ -28,13 +28,23 @@
 #define ID_EDIT                         40012
 #define ID_INFO                         40013
 #define ID_6_3_MENU                     40014
+#define ID_40015                        40015
+#define ID_40016                        40016
+#define ID_40017                        40017
+#define ID_40018                        40018
+#define ID_40019                        40019
+#define ID_40020                        40020
+#define ID_40021                        40021
+#define ID_40022                        40022
+#define ID_40023                        40023
+#define ID_40024                        40024
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        109
-#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_COMMAND_VALUE         40025
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
