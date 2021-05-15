acc66957b293219aec787be7cf3e738aa52d4ba1
lja3723 <lja3723@gmail.com>
2021-05-15 16:29:35 +0900

    mod ex5-5

--- ex5-5(더블버퍼링).cpp
+++ ex5-5(더블버퍼링).cpp
@@ -56,6 +56,21 @@ void TextPrint(HDC hdc, int x, int y, TCHAR text[])
 	TextOut(hdc, x, y, text, _tcslen(text));
 }
 
+BOOL IsTextInOfRect(HWND hwnd, HDC hdc, TCHAR text[], int x, int y)
+{
+	BOOL result = FALSE;
+	RECT rt_hwnd;
+	SIZE size;
+	GetClientRect(hwnd, &rt_hwnd);
+	GetTextExtentPoint(hdc, text, _tcslen(text), &size);
+	if (rt_hwnd.left < x && x + size.cx < rt_hwnd.right)
+		result = TRUE;
+	if (rt_hwnd.top < y && y + size.cy < rt_hwnd.bottom)
+		result = TRUE;
+
+	return result;
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	static HINSTANCE hInstance;
@@ -66,8 +81,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	RECT rt_hwnd;
 
 	TCHAR word[] = _T("내년이 올까?");
+	SIZE szWord;
+
 	static int xPos = 0;
 	static int yPos = 0;
+	static int xSpeed = 0;
+	static int ySpeed = 0;
+	static int accelVal = 1;
+	static int keyFlag[4] = { 0, };
 
 	switch (iMsg) {
 	case WM_CREATE:
@@ -75,7 +96,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		hBitmap[1] = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP12));
 		GetObject(hBitmap[1], sizeof(BITMAP), &bitmap);
 
-		SetTimer(hwnd, 1, 15, NULL);
+		SetTimer(hwnd, 1, 30, NULL);
 		return 0;
 
 	case WM_PAINT:
@@ -84,11 +105,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		memdc[1] = CreateCompatibleDC(memdc[0]);
 		if (hBitmap[0] == NULL)
 			hBitmap[0] = CreateCompatibleBitmap(hdc, bitmap.bmWidth, bitmap.bmHeight);
+		GetClientRect(hwnd, &rt_hwnd);
 
 		oldBitmap[0] = (HBITMAP)SelectObject(memdc[0], hBitmap[0]);
 		oldBitmap[1] = (HBITMAP)SelectObject(memdc[1], hBitmap[1]);
-
-		GetClientRect(hwnd, &rt_hwnd);
 		BitBlt(memdc[0], 0, 0, rt_hwnd.right, rt_hwnd.bottom, memdc[1], 0, 0, SRCCOPY);
 		SetBkMode(memdc[0], TRANSPARENT);
 		TextPrint(memdc[0], xPos, yPos, word);
@@ -100,14 +120,67 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		DeleteDC(memdc[1]);
 		EndPaint(hwnd, &ps);
 		return 0;
-
+	/*
 	case WM_MOUSEMOVE:
 		xPos = LOWORD(lParam);
 		yPos = HIWORD(lParam);
 		return 0;
+		*/
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_LEFT:
+			keyFlag[0] = 1;
+			break;
+		case VK_RIGHT:
+			keyFlag[1] = 1;
+			break;
+		case VK_UP:
+			keyFlag[2] = 1;
+			break;
+		case VK_DOWN:
+			keyFlag[3] = 1;
+			break;
+		}
+		return 0;
+	case WM_KEYUP:
+		switch (wParam)
+		{
+		case VK_LEFT:
+			keyFlag[0] = 0;
+			break;
+		case VK_RIGHT:
+			keyFlag[1] = 0;
+			break;
+		case VK_UP:
+			keyFlag[2] = 0;
+			break;
+		case VK_DOWN:
+			keyFlag[3] = 0;
+			break;
+		}
+		return 0;
 	case WM_TIMER:
-		//GetClientRect(hwnd, &rt_hwnd);
 		//if (ypos > rt_hwnd.bottom) ypos = 0;
+		if (keyFlag[0])
+			xSpeed -= accelVal;
+		if (keyFlag[1])
+			xSpeed += accelVal;
+		if (keyFlag[2])
+			ySpeed -= accelVal;
+		if (keyFlag[3])
+			ySpeed += accelVal;
+		xPos += xSpeed;
+		yPos += ySpeed;
+
+		hdc = GetDC(hwnd);
+		if (!IsTextInOfRect(hwnd, hdc, word, xPos, yPos))
+		{
+			xPos = yPos = 0;
+			xSpeed = ySpeed = 0;
+		}
+		ReleaseDC(hwnd, hdc);
+
 		InvalidateRgn(hwnd, NULL, FALSE);
 		return 0;
 
